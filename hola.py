dd05f0b681c059951a12b61faa258681d50b3ec0
M1xeeee <josephsoto690@gmail.com>
2025-04-15 16:24:15 -0400

    commit

--- hola.py
+++ hola.py
@@ -17,12 +17,34 @@
 
 # # Multiplicacion
 
-n1=int(input("ingrese un numero "))
-n2=int(input("ingrese un numero "))
-print("La multiplacion de estos numeros da", n1*n2)
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+# print("La multiplacion de estos numeros da", n1*n2)
 
 
+# # Promedio de 3 numeros
 
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+# n3=int(input("ingrese un numero "))
+# n4=n1+n2+n3
 
+# if n4>=40:
+    # print("el alumno aprobo")
+# else:
+    # print("el alumno reprobo")
 
+# print("El promedio es", n4/3)
 
+## Edad ingresar 
+
+edad=int(input("ingrese su edad"))
+if edad>=18:
+    print("usted es mayor de edad")
+
+elif edad>=12 and edad=<18:
+    print("Usted es adolescente")
+elif edad>=18 and edad=<65:
+    print("Usted es un adulto")
+else edad>65
+    print("Usted es un viejo")
