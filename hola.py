530426a66802d992793caa71ed1360846f5a77b6
M1xeeee <josephsoto690@gmail.com>
2025-04-15 15:38:19 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,28 @@
+
+
+# # Ingreso de variables
+
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+
+# print("holaaaa ,",nombre, "y su edad es", edad)
+
+# # Suma
+
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+# print(n1+n2)
+
+# # Multiplicacion
+
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+print("La multiplacion de estos numeros da", n1*n2)
+
+
+
+
+
+
