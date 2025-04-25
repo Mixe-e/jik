cc8e70d595aba61714dde253008099cc306de56a
Mixe-e <josephsoto690@gmail.com>
2025-04-25 18:33:49 -0400

    commit

---
+++ street fighter.py
@@ -0,0 +1,5 @@
+import random 
+randy=random.randint(2,10)
+print(randy)
+
+
--- supermercado.py
+++ supermercado.py
@@ -1,33 +1,36 @@
-print("bienvenido al supermercado")
-jugo=10
-ak=5
-pistola=1
+total = 0
+palta=3000
+tomate=2000
+naranja=1500
+pera=300
+Ak47=100000
 
-escritura = input
-usuario= input
-total=0
+while True:
+    print("1. Palta")
+    print("2. Tomate")
+    print("3. Naranja")
+    print("4. Pera")
+    print("5. Ak-47")
+    print("6. Salir")
+    opcion = int(input("Ingrese una opción (1-5): "))
 
-
-while usuario!=4:
-    print("Que va a llevar?")
-    print("1.ak")
-    print("2.pistola")
-    print("3.jugo de manzana")
-    print(input(escritura))
-    if escritura==3:
-        print("te has llevado una ak")
-        total=total+ak
-    elif escritura==2:
-        print("te llevaste una pistola")
-        total=total+pistola
-    elif escritura==1:
-        print("te llevaste un jugo de manzana")
-        total=total+jugo
+    if opcion == 1:
+        print("Llevaste 1 palta")
+        total=total+palta
+    elif opcion == 2:
+        print("Llevaste 1 tomate")
+        total=total+tomate
+    elif opcion == 3:
+        print("Llevaste 1 naranja")
+        total=total+naranja
+    elif opcion == 4:
+        print("Llevaste 1 pera")
+        total=total+pera
+    elif opcion == 5:
+        print("Llevaste 1 Ak-47")
+        total=total+Ak47
+    elif opcion == 6:
+        break
     else:
-        print("Di algo valido")
-
-print("Gracias por comprar, el recibo:")
-print("Pistola: ", pistola)
-print("ak: ", ak)
-print("jugo: ", jugo)
-print("Total: ", total)
+        print("Opción no válida")
+    opcion = int(input("Ingrese una opción (1-6): "))
