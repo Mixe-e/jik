b772be251037f306e749e8e1d1b0dcad81d0d27d
Mixe-e <josephsoto690@gmail.com>
2025-04-29 15:05:23 -0400

    commit

--- street fighter.py
+++ street fighter.py
@@ -1,5 +1,33 @@
 import random 
 randy=random.randint(2,10)
 print(randy)
+street=50
+street2=50
+a=0
+while street>1 and street2>0:
+    if a % 2 == 0:
+            print("Ejecutar golpe")
+            a=a+1
+    else:
+            print("Ejecutar golpe2")
+            a=a+1
 
 
+# input () -> retorna un str ingresado por consola
+# nombre = input ("Ingrese su nombre")
+# edad=int() -> retorna un integer , str o number
+## edad = int("71")
+## ** potencia/ elevado a
+## % modulo/ MOD 
+## / Division
+## + Suna
+## - Resta
+## * Multiplicacion
+
+
+## Pedir edad
+# edad = int(input("Ingrese su edad:"))
+# a=float(1)
+# print(a)
+# if(a<=b):
+#     print("a es menor que b")
