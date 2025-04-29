af2dc6739c8a78c70852e23116595de91e1d3fd9
Mixe-e <josephsoto690@gmail.com>
2025-04-29 15:32:04 -0400

    commit

--- street fighter.py
+++ street fighter.py
@@ -8,11 +8,27 @@ while street>1 and street2>0:
     if a % 2 == 0:
             print("Ejecutar golpe")
             a=a+1
+            randy=random.randint(2,10)
+            street=street-randy
+            print("Tu golpe hizo", randy)
+            print("--------------------")
+            print("Vida de street fighter 1: ", street)
     else:
             print("Ejecutar golpe2")
             a=a+1
+            randy=random.randint(2,10)
+            street2=street2-randy
+            print("Tu golpe hizo", randy)
+            print("--------------------")
+            print("Vida de street fighter 2: ", street2)
 
 
+if street<1:
+    print("Street 2 gana")
+else:
+    print("Street 1 gana")
+
+            
 # input () -> retorna un str ingresado por consola
 # nombre = input ("Ingrese su nombre")
 # edad=int() -> retorna un integer , str o number
@@ -31,3 +47,8 @@ while street>1 and street2>0:
 # print(a)
 # if(a<=b):
 #     print("a es menor que b")
+
+## cont+=1 <- contador simplificado
+
+## for i in range(10):
+
