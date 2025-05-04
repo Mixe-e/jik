aa8729deffc4694424751c79c09fb4cd4232dcbb
Mixe-e <josephsoto690@gmail.com>
2025-05-03 21:47:58 -0400

    commit

--- Ludopatear/Tragamonedas.py
+++ Ludopatear/Tragamonedas.py
@@ -26,13 +26,18 @@ while tragamonedas != 4:
         print(Slot3)
         time.sleep(0.10)
     if Slot1==Slot2==Slot3:
-        print(Fore.GREEN, "GANASTE!")
+        print(Fore.GREEN, "3 Iguales | GANASTE!")
         print(Fore.YELLOW, "+1000 Creditos")
         Creditos=Creditos+1000
     if Slot1==Slot2 or Slot2==Slot3 or Slot1==Slot3:
-        print(Fore.GREEN, "GANASTE!")
-        print(Fore.YELLOW, "+100 Creditos")
-        Creditos=Creditos+100
+        print(Fore.GREEN, "2 Iguales | GANASTE!")
+        print(Fore.YELLOW, "+50 Creditos")
+        Creditos=Creditos+50
+    if Slot1==1 and Slot2==2 and Slot3==3 or Slot1==4 and Slot2==5 and Slot3==6 or Slot1==7 and Slot2==8 and Slot3==9:
+        print(Fore.GREEN, "3 en linea | GANASTE!")
+        print(Fore.YELLOW, "+5000 Creditos")
+        Creditos=Creditos+5000
+    
     if tragamonedas==4:
         print(Fore.LIGHTYELLOW_EX, "Saliendo del programa...")
     
