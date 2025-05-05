70ac46c68315fc2e0326ce9d1c4edde99deaacca
Mixe-e <josephsoto690@gmail.com>
2025-05-05 11:36:45 -0400

    commit

--- Ludopatear/Tragamonedas.py
+++ Ludopatear/Tragamonedas.py
@@ -10,17 +10,69 @@ tragamonedas="unboton"
 GIG=0
 KIK=0
 JIK=0
-
+apuesta=0
 
 nombre=(input("Ingresa tu nombre de inversor/a experto"))
 
 while tragamonedas != 4:
     print("Tus creditos:",Fore.YELLOW, Creditos)
+    print("Si quieres aumentar/bajar la apuesta escribe: 1")
     print("----------------------------------------")
     tragamonedas=(input("Pulsa cualquier boton para tirar..."))
-    if Creditos>=100:  
+
+    while tragamonedas==1:
+        print(Fore.YELLOW, "Ajusta tu apuesta")
+        apuesta=(input(Fore.YELLOW, """
+              1. 100 Creditos
+              2. 200 Creditos
+              3. 500 Creditos
+              4. 1000 Creditos
+              5. 2000 Creditos
+              6. 5000 Creditos
+              """))
+        if apuesta==1:
+            if Creditos>= 100:
+                print(Fore.CYAN, "Apuesta ajustada a 100 Creditos")
+                apuesta=100
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+        if apuesta==2:
+            if Creditos>= 200:
+                print(Fore.CYAN, "Apuesta ajustada a 200 Creditos")
+                apuesta=200
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+        if apuesta==3:
+            if Creditos>= 500:
+                print(Fore.CYAN, "Apuesta ajustada a 500 Creditos")
+                apuesta=500
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+        if apuesta==4:
+            if Creditos>= 1000:
+                print(Fore.CYAN, "Apuesta ajustada a 1000 Creditos")
+                apuesta=1000
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+        if apuesta==5:
+            if Creditos>= 2000:
+                print(Fore.CYAN, "Apuesta ajustada a 2000 Creditos")
+                apuesta=2000
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+        if apuesta==6:
+            if Creditos>= 5000:
+                print(Fore.CYAN, "Apuesta ajustada a 5000 Creditos")
+                apuesta=5000
+        else:
+            print(Fore.RED, "No tienes suficientes creditos para realizar esta apuesta")
+
+
+
+
+    if Creditos>=apuesta and tragamonedas=="":
         
-        Creditos=Creditos-100
+        Creditos=Creditos-apuesta
         for i in range(lanzando):
             Slot1=random.randint(1,10)
             print(Slot1)
@@ -48,18 +100,18 @@ while tragamonedas != 4:
         print(Slot3)
     if Slot1==Slot2==Slot3:
         print(Fore.GREEN, "3 Iguales | GANASTE!")
-        print(Fore.YELLOW, "+1000 Creditos")
-        Creditos=Creditos+1000
+        print(Fore.YELLOW, "+", apuesta*5, " Creditos")
+        Creditos=Creditos+(apuesta*5)
         KIK=KIK+1
     if Slot1==Slot2 or Slot2==Slot3 or Slot1==Slot3:
         print(Fore.GREEN, "2 Iguales | GANASTE!")
-        print(Fore.YELLOW, "+50 Creditos")
-        Creditos=Creditos+50
+        print(Fore.YELLOW, "+", apuesta/2, " Creditos")
+        Creditos=Creditos+(apuesta/2)
         GIG=GIG+1
     if Slot1==1 and Slot2==2 and Slot3==3 or Slot1==4 and Slot2==5 and Slot3==6 or Slot1==7 and Slot2==8 and Slot3==9:
         print(Fore.GREEN, "3 en linea | GANASTE!")
-        print(Fore.YELLOW, "+5000 Creditos")
-        Creditos=Creditos+5000
+        print(Fore.YELLOW, "+", apuesta*10, " Creditos")
+        Creditos=Creditos+(apuesta*10)
         JIK=JIK+1
     
     if tragamonedas==4:
@@ -86,10 +138,10 @@ if JIK>1:
     print(Fore.YELLOW, "3 En linea:", JIK)
 
 print(Fore.GREEN, "------------------")
-LOP=(GIG * 50) + ( JIK * 5000 ) + (KIK*1000) 
+LOP=(GIG * apuesta) + ( JIK * apuesta ) + (KIK*apuesta) 
 print(Fore.YELLOW, "Tus puntos:", LOP)
 
-if LOP>5000:
+if LOP>10000:
     print(Fore.YELLOW, "Tabla de posiciones")
     print(Fore.CYAN, "1.", nombre, ":",  LOP, "puntos")
     print(Fore.MAGENTA, "2. Mixin : 10000 puntos")
