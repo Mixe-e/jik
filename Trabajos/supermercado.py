8099ae326d5c818d4d74d611c1831ec9c3aac487
Mixe-e <josephsoto690@gmail.com>
2025-05-16 17:05:28 -0400

    commit

--- Trabajos/supermercado.py
+++ Trabajos/supermercado.py
@@ -4,33 +4,61 @@ tomate=2000
 naranja=1500
 pera=300
 Ak47=100000
+op=0
+name="sinnombre"
+nombresaso=False
+def supermercado():
+    
+        while True:
+            try:
+                print("""
+                      1. Palta
+                      2. Tomate
+                      3. Naranja
+                      4. Pera
+                      5. AK47
+                      6. Salir      
+                      """)
+                op = int(input("Ingrese una opción (1-5): "))
 
-while True:
-    print("1. Palta")
-    print("2. Tomate")
-    print("3. Naranja")
-    print("4. Pera")
-    print("5. Ak-47")
-    print("6. Salir")
-    opcion = int(input("Ingrese una opción (1-5): "))
+                match op:
+                    
+                    case 1:    
+                        print("Llevaste 1 palta")
+                        total=total+palta
+                        pa=pa+1
+                    case 2:
+                        print("Llevaste 1 tomate")
+                        total=total+tomate
+                    case 3:
+                        print("Llevaste 1 naranja")
+                        total=total+naranja
+                    case 4:
+                        print("Llevaste 1 pera")
+                        total=total+pera
+                    case 5:
+                        print("Llevaste 1 Ak-47")
+                        total=total+Ak47
+                    case 6:
+                        break
+                    case 7:
+                        name=input("Escriba su nombre")
+                        nombresaso=True
+                    case _:
+                        print("Opción no válida")
+            except Exception:
+                print("Solo numeros no texto")
 
-    if opcion == 1:
-        print("Llevaste 1 palta")
-        total=total+palta
-    elif opcion == 2:
-        print("Llevaste 1 tomate")
-        total=total+tomate
-    elif opcion == 3:
-        print("Llevaste 1 naranja")
-        total=total+naranja
-    elif opcion == 4:
-        print("Llevaste 1 pera")
-        total=total+pera
-    elif opcion == 5:
-        print("Llevaste 1 Ak-47")
-        total=total+Ak47
-    elif opcion == 6:
-        break
-    else:
-        print("Opción no válida")
-    opcion = int(input("Ingrese una opción (1-6): "))
+
+supermercado()
+
+
+def salida():
+    print("Total:", total)
+
+
+if nombresaso==True:
+    print("Gracias por su compra", name)
+
+
+salida()
