008467fb52d920b469000427d949c5e047953015
Mixe-e <josephsoto690@gmail.com>
2025-08-14 09:36:37 -0400

    blublublubl

---
+++ Matematica aplicada/guia 0.py
@@ -0,0 +1,30 @@
+## Ejercicio 1
+
+# while True:
+#     try:
+#         num=int(input("Ingrese un numero "))
+#         if num>0:
+#             print("Tu numero es positivo")
+#         elif num<0:
+#             print("Tu numero es negativo")
+#         elif num==0:
+#             print("Tu numero es 0")
+#     except Exception:
+#         print("Coloca solo numeros")    
+
+
+## Ejercicio 2
+
+# while True:
+#     try:
+#         num=int(input("Ingrese un numero "))
+#         if num % 2 == 0:
+#             print("Tu numero es par")
+#         else:
+#             print("Tu numero es impar")
+#     except Exception:
+#         print("Coloca solo numeros")    
+
+
+## Ejercicio 3
+
